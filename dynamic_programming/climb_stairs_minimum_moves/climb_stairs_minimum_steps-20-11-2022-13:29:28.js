6811214cbfe3de1bf5f54109b66fb8726458d525
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-11-20 19:53:08 +0530

    [KATA]: Climb Stairs Dynamic programming with Specified Jums

--- dynamic_programming/climb_stairs_minimum_moves/climb_stairs_minimum_steps-20-11-2022-13:29:28.js
+++ dynamic_programming/climb_stairs_minimum_moves/climb_stairs_minimum_steps-20-11-2022-13:29:28.js
@@ -7,4 +7,24 @@
      staircase.
 Note -> If there is no path through the staircase print null.
  */
+const findMinimumPath = (jumpsAllowedArray) => {
+    const n = jumpsAllowedArray.length;
+    const tabulator = Array(jumpsAllowedArray.length + 1).fill(null);
+    tabulator[jumpsAllowedArray.length] = 0;
+    
+    for(let i = n-1; i >= 0; i--){
+        if(jumpsAllowedArray[i] > 0){
+            let minCost =  Number.MAX_VALUE;
+            for(let j = 1; j <= jumpsAllowedArray[i] && i + j < tabulator.length; j++){
+                if(tabulator[i + j] !== null) {
+                    minCost = Math.min(minCost, tabulator[i + j]);
+                }
 
+                
+                if(minCost !== Number.MAX_VALUE ) tabulator[i] = minCost + 1;
+            }
+        }
+    }
+    console.log(tabulator[0]);
+}
+findMinimumPath([1,1,1,1,1,1,1])
