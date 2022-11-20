5e29689020158ae635e369274e254496b5ffc1c8
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-11-20 13:30:04 +0530

    Setting up Minimum Stairs

---
+++ dynamic_programming/climb_stairs_minimum_moves/climb_stairs_minimum_steps-20-11-2022-13:29:28.js
@@ -0,0 +1,10 @@
+/**
+ * 1. You are given a number n, representing the number of stairs in a staircase.
+2. You are on the 0th step and are required to climb to the top.
+3. You are given n numbers, where ith element's value represents - till how far from the step you 
+     could jump to in a single move.  You can of-course fewer number of steps in the move.
+4. You are required to print the number of minimum moves in which you can reach the top of 
+     staircase.
+Note -> If there is no path through the staircase print null.
+ */
+
