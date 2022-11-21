c9758c4ba759fd2066a171e0beab62ab4b04f69b
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-11-21 23:46:10 +0530

    Setting up minimum maze travel

---
+++ dynamic_programming/min_cost_maze_travel/min_cost_maze_travel-21-11-2022-09:46:33.js
@@ -0,0 +1,17 @@
+/**
+ * 1. You are given a number n, representing the number of rows.
+2. You are given a number m, representing the number of columns.
+3. You are given n*m numbers, representing elements of 2d array a, which represents a maze.
+4. You are standing in top-left cell and are required to move to bottom-right cell.
+5. You are allowed to move 1 cell right (h move) or 1 cell down (v move) in 1 motion.
+6. Each cell has a value that will have to be paid to enter that cell (even for the top-left and bottom- 
+     right cell).
+7. You are required to traverse through the matrix and print the cost of path which is least costly.
+ */
+
+// Algo:
+/**
+ * 1. Assign Memory
+ * 2. decide meaning of the memory
+ * 
+ */
