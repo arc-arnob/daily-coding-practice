242acf2a7e3729f70c4873385a3466cbcb1a3de1
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-12-04 08:52:40 +0530

    Added Display to linked list

--- linked_list/linked_list_add_element-03-12-2022-22:34:45.js
+++ linked_list/linked_list_add_element-03-12-2022-22:34:45.js
@@ -22,7 +22,10 @@ class LinkedList {
     add(data){
         let node = new Node(data);
         let current;
-        if(this.head === null) this.head = node;
+        if(this.head === null){
+            console.log("Called")
+            this.head = node;
+        }
         else{
             current = this.head;
             while(current.next){
@@ -32,4 +35,21 @@ class LinkedList {
         }
         this.size++
     }
+    display(){
+        let current = this.head;
+        while(current.next){
+            console.log(current);
+            current = current.next;
+        }
+    }
 }
+
+// Driver
+const inputs = [1,2,3,4,5,6,7,8,9,10];
+
+const linkedList = new LinkedList();
+
+for(let i = 0; i < inputs.length; i++){
+    linkedList.add(inputs[i]);
+}
+ linkedList.display();
