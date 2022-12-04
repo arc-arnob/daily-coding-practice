2318c1ee8ed1b524e1c96ebb91205a2a343a5b0f
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2022-12-04 09:29:08 +0530

    KATA: Linked List add Remove element from front of Linked List

--- linked_list/linked_list_add_element-03-12-2022-22:34:45.js
+++ linked_list/linked_list_add_element-03-12-2022-22:34:45.js
@@ -43,4 +43,14 @@ class LinkedList{
     }
 }
 
-export default LinkedList;
+// const LinkedList = require("./linked_list_add_element-03-12-2022-22:34:45")
+
+// Driver
+const inputs = [1,2,3,4,5,6,7,8,9,10];
+
+const linkedList = new LinkedList();
+
+for(let i = 0; i < inputs.length; i++){
+    linkedList.add(inputs[i]);
+}
+linkedList.display();
--- linked_list/linked_list_remove_element_from_first-04-12-2022-09:00:29.js
+++ linked_list/linked_list_remove_element_from_first-04-12-2022-09:00:29.js
@@ -1,4 +1,53 @@
-import  LinkedList from "./linked_list_add_element-03-12-2022-22:34:45";
+/**
+ * Node -> 
+ * {
+ *  Data,
+ *  Next
+ * }
+ */
+
+ class Node {
+    constructor(element)
+    {
+        this.element = element;
+        this.next = null
+    }  
+}
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    add(data){
+        let node = new Node(data);
+        let current;
+        if(this.head === null){
+            console.log("Called")
+            this.head = node;
+        }
+        else{
+            current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++
+    }
+    removeFromHead(){
+        if(this.head === null) return;
+        let current = this.head;
+        this.head = current.next
+        current.next = null;
+    }
+    display(){
+        let current = this.head;
+        while(current.next){
+            console.log(current);
+            current = current.next;
+        }
+    }
+}
 
 // const LinkedList = require("./linked_list_add_element-03-12-2022-22:34:45")
 
@@ -10,4 +59,7 @@ const linkedList = new LinkedList();
 for(let i = 0; i < inputs.length; i++){
     linkedList.add(inputs[i]);
 }
+linkedList.display();
+console.log("*********END*********")
+linkedList.removeFromHead();
 linkedList.display();
