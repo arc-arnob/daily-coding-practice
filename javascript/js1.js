0d46458cbbf732f5e5523a39419eea9f2d599c5f
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2023-01-13 20:28:05 +0530

    No impr

--- javascript/js1.js
+++ javascript/js1.js
@@ -20,25 +20,22 @@ const actualInput = [
   {
     name: "abc",
   },
-  {
-    name: "bced",
-  },
-  {
-    name: "bced",
-  },
+
 ]
 const countUniques = (actualInput) => {
   let previousValue = actualInput[0];
+  let incremental_str = 0;
   const output = actualInput.reduce((accumulator, currentValue) => {
     if (accumulator[currentValue.name] && previousValue.name === currentValue.name) {
       accumulator[currentValue.name].count += 1;
     } else {
       if (Object.keys(accumulator).includes(currentValue.name)) {
-        accumulator[currentValue.name + '_rep_rm'] = { name: currentValue.name, count: 1 }
-        accumulator[currentValue.name + '_rep_rm'].count = 1;
+        accumulator[currentValue.name + '_rep_rm' + incremental_str] = { name: currentValue.name, count: 1 }
+        accumulator[currentValue.name + '_rep_rm' + incremental_str].count = 1;
       }
       else {
         accumulator[currentValue.name] = { name: currentValue.name, count: 1 }
+        incremental_str += 1;
       }
     }
     previousValue = currentValue;
@@ -48,7 +45,7 @@ const countUniques = (actualInput) => {
   for (ele in output) {
     finalOutput.push(output[ele]);
   }
-  console.log(finalOutput);
+  console.log(output);
 }
 countUniques(actualInput);
 
