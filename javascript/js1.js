db992b72a3800aef865e76187f5c9f2d5125fb11
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2023-01-13 12:00:44 +0530

    Made improvements

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,32 +1,63 @@
 const actualInput = [
-{
-  	name: "abc",
+  {
+    name: "abc",
   },
   {
-  	name: "abc",
+    name: "abc",
   },
   {
-  	name: "abc",
+    name: "abc",
   },
   {
-  	name: "bce",
+    name: "bce",
   },
   {
-  	name: "bced",
+    name: "bced",
   },
   {
-  	name: "abc",
-  }
+    name: "abc",
+  },
+  {
+    name: "abc",
+  },
+  {
+    name: "bced",
+  },
+  {
+    name: "bced",
+  },
 ]
 const countUniques = (actualInput) => {
-	const output = actualInput.reduce((accumulator, currentValue) => {
-  		if(accumulator[currentValue.name]){
-      	accumulator[currentValue.name]+=1;
-      }else{
-        accumulator[currentValue.name] = 1
+  let previousValue = actualInput[0];
+  const output = actualInput.reduce((accumulator, currentValue) => {
+    if (accumulator[currentValue.name] && previousValue.name === currentValue.name) {
+      accumulator[currentValue.name].count += 1;
+    } else {
+      if (Object.keys(accumulator).includes(currentValue.name)) {
+        accumulator[currentValue.name + '_rep_rm'] = { name: currentValue.name, count: 1 }
+        accumulator[currentValue.name + '_rep_rm'].count = 1;
       }
-      return accumulator;
+      else {
+        accumulator[currentValue.name] = { name: currentValue.name, count: 1 }
+      }
+    }
+    previousValue = currentValue;
+    return accumulator;
   }, []);
-  console.log(output);
+  const finalOutput = [];
+  for (ele in output) {
+    finalOutput.push(output[ele]);
+  }
+  console.log(finalOutput);
 }
 countUniques(actualInput);
+
+// Expected Output: 
+/**
+ * [
+ *  {
+ *  name: abc,
+ *  count: 3 
+ * }
+ * ]
+ */
