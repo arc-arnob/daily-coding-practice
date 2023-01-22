cab55b556cee9106b737dbfd35140619b5c3ec1d
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2023-01-23 00:11:23 +0530

    Added Object.is

--- javascript/implement-object-is-22-01-2023-10:29:20.js
+++ javascript/implement-object-is-22-01-2023-10:29:20.js
@@ -8,3 +8,15 @@ Object.is(NaN, NaN) // true
 NaN === NaN // false
 Here is the detailed spec, can you implement your own is()?
  */
+
+function is(a, b) {
+    if (a !== a) { // Only NaN is not equal to itself
+        return b !== b; // returns true if the second parameter is NaN too
+    }
+
+    if (a === 0 && b === 0) { // -0 === 0 is true, so when both parameters equals to 0
+        return 1 / a === 1 / b; // 1 / -0 is -Infinity and -Infinity === -Infinity
+    }
+
+    return a === b; // All other cases with regular === comparison
+}
