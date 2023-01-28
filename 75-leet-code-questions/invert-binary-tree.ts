e8c459f849be9ba5e35faba485243c506425436a
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2023-01-28 23:11:21 +0530

    Added Code for Inverting a binary tree

---
+++ 75-leet-code-questions/invert-binary-tree.ts
@@ -0,0 +1,20 @@
+import { BinaryTree, TreeNodeStructure } from "./tree-setup"
+
+// Invert a binary tree
+const invertBinaryTree = (node: TreeNodeStructure) => {
+    if (!node) return;
+    [node.left, node.right] = [node.right, node.left];
+    invertBinaryTree(node.left);
+    invertBinaryTree(node.right);
+}
+
+const binaryTree = new BinaryTree();
+binaryTree.insert(10);
+binaryTree.insert(5);
+binaryTree.insert(15);
+binaryTree.insert(2);
+binaryTree.insert(7);
+binaryTree.insert(12);
+binaryTree.insert(20);
+
+binaryTree.display();
---
+++ 75-leet-code-questions/tree-setup.js
@@ -0,0 +1,55 @@
+var TreeNode = /** @class */ (function () {
+    function TreeNode(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    return TreeNode;
+}());
+var BinaryTree = /** @class */ (function () {
+    function BinaryTree() {
+        this.root = null;
+    }
+    BinaryTree.prototype.insert = function (value) {
+        // Create a New Node
+        // if root is null, make new node as root and return
+        // else check left child and right child
+        // if no left child, append as left child
+        // else append as right child
+        var newNode = new TreeNode(value);
+        if (!this.root) {
+            this.root = newNode;
+            return;
+        }
+        var currentNode = this.root;
+        while (true) {
+            if (value < currentNode.value) {
+                // Move to left
+                if (!currentNode.left) {
+                    currentNode.left = newNode;
+                    break;
+                }
+                currentNode = currentNode.left;
+            }
+            else {
+                // Move to right
+                if (!currentNode.right) {
+                    currentNode.right = newNode;
+                    break;
+                }
+                currentNode = currentNode.right;
+            }
+        }
+    };
+    BinaryTree.prototype.display = function () {
+        this.inOrderTraversal(this.root);
+    };
+    BinaryTree.prototype.inOrderTraversal = function (node) {
+        if (!node)
+            return;
+        this.inOrderTraversal(node.left);
+        console.log(node.value);
+        this.inOrderTraversal(node.right);
+    };
+    return BinaryTree;
+}());
