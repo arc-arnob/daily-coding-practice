b6261d01aa75ef048381d9560f0338bdbb3d4484
Arnob  Chowdhury <arnobchowdhury@Arnobs-MacBook-Pro-2.local>
2023-01-31 23:07:48 +0530

    Added Improvement

--- 75-leet-code-questions/lowest-common-ancestor.ts
+++ 75-leet-code-questions/lowest-common-ancestor.ts
@@ -3,6 +3,8 @@ import { TreeNodeStructure } from "./tree-setup";
 const lowestCommonAncestor =
     (root: TreeNodeStructure | null, p: TreeNodeStructure | null, q: TreeNodeStructure | null)
         : TreeNodeStructure | null => {
+        if (p?.right?.value === q.value || p?.left?.value === q.value) return p;
+        if (q?.right?.value === p.value || q?.left?.value === p.value) return q;
         if (root.value > p.value && root.value > q.value) {
             lowestCommonAncestor(root.left, p, q);
         } else if (root.value < p.value && root.value < q.value) {
