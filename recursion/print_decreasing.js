1540d89c3c67185f137bd53d7aa9541b16aeffee
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-07-01 16:52:26 +0530

    One arg recursion

--- recursion/print_decreasing.js
+++ recursion/print_decreasing.js
@@ -1,9 +1,13 @@
 const printDecreasing = (n) => {
-    if(n === 0 ){
+    if (n === 0) {
         return;
     }
     console.log(n);
-    printDecreasing(n-1);
+    printDecreasing(n - 1);
 }
 
 printDecreasing(5);
+
+// Expectations
+// Faith
+// Meet
--- recursion/print_increasing_decreasing.js
+++ recursion/print_increasing_decreasing.js
@@ -1,10 +1,21 @@
 const printDecreasing = (n, i) => {
-    if( i > n ){
+    if (i > n) {
         return;
     }
     console.log(i);
-    printDecreasing(n, i+1);
+    printDecreasing(n, i + 1);
     console.log(i);
 }
 
-printDecreasing(5, 1);
+const printIncDecOneArg = (n) => {
+    if (n == 0) {
+        return;
+    }
+    console.log(n);
+    printIncDecOneArg(n - 1);
+    console.log(n);
+}
+
+// printDecreasing(5, 1);
+
+printIncDecOneArg(5);
