4738c10e54e9ea5230cc97dbe55df80bb0852723
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-08-26 17:15:04 +0200

    Added leet code ques for arrays, duplicate not yet removed

---
+++ leet_code_questions/dist/three_sum.js
@@ -0,0 +1,59 @@
+/**
+ * https://leetcode.com/problems/3sum/
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+ */
+function threeSum(nums) {
+    var result = [];
+    nums.sort(function (a, b) { return a - b; });
+    for (var i = 0; i < nums.length; i++) {
+        var nums_i = nums[i];
+        var j = i + 1;
+        var k = nums.length - 1;
+        while (j < k) {
+            console.log(nums_i, nums[j], nums[k]);
+            if (nums[j] + nums[k] + nums_i === 0) {
+                result.push([nums_i, nums[j], nums[k]]);
+                j++;
+                k--;
+            }
+            else {
+                k--;
+            }
+        }
+    }
+    return result;
+}
+;
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([0, 1, 1]));
